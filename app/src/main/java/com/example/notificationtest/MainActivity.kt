3622f78dfa6013f66cd067f3afacb311e0f6c50f
2Bextreme <yamaguti0623@gmail.com>
2021-10-29 14:22:44 +0900

    a

--- app/src/main/java/com/example/notificationtest/MainActivity.kt
+++ app/src/main/java/com/example/notificationtest/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.notificationtest
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -13,61 +16,47 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.work.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val CHANNEL_ID = "213412"
-        private const val Long = 12
     }
 
-    private lateinit var setTime:Date
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createNotificationChannel()
 
-        val textTitle = "テスト"
-        val textContent = "テスト通知"
-        val notificationId = 1
+        val uploadWorkRequest: WorkRequest =
+            PeriodicWorkRequestBuilder<UploadWorker>(5,TimeUnit.SECONDS)
+                .setInitialDelay(5,TimeUnit.SECONDS)
+                .build()
 
-        val intent = Intent(this, SubActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
-        }
-        val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(textTitle)
-            .setContentText(textContent)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        fun popNotifi(time :String) {
-            with(NotificationManagerCompat.from(this)) {
-                notify(notificationId, builder.build())
-            }
-        }
+//        val alarm = AlarmManager.AlarmClockInfo().apply {
+//            this.showIntent(pendingIntent)
+//            this.triggerTime()
+//        }
 
         val button: Button = findViewById(R.id.notificationBtn)
         button.setOnClickListener {
             val test: EditText = findViewById(R.id.editTextDate2)
+            createNotificationChannel()
             Toast.makeText(applicationContext, test.toString(), Toast.LENGTH_SHORT).show()
-                popNotifi(test.text.toString())
-
+            WorkManager
+                .getInstance(applicationContext)
+                .enqueue(uploadWorkRequest)
         }
     }
-
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
+            val name = "テストチャンネル"
+            val descriptionText = "テスト通知"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
                 description = descriptionText
@@ -79,3 +68,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
---
+++ app/src/main/java/com/example/notificationtest/UploadWorker.kt
@@ -0,0 +1,53 @@
+package com.example.notificationtest
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(appContext: Context, workerParams: WorkerParameters):
+    Worker(appContext, workerParams){
+
+
+
+    override fun doWork(): Result {
+
+        val textTitle = "テスト"
+        val textContent = "テスト通知"
+        val notificationId = 1
+
+        val intent = Intent(applicationContext, SubActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(applicationContext, "213412")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(textTitle)
+            .setContentText(textContent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        fun popNotification() {
+            with(NotificationManagerCompat.from(applicationContext)) {
+                notify(notificationId, builder.build())
+            }
+        }
+        popNotification()
+        // Indicate whether the work finished successfully with the Result
+        return Result.success()
+    }
+
+}
+
